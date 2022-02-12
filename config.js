38afd2cfc6804c0394766d12f14c42bbd4d08ec3
L'Patée <86246829+lpatee@users.noreply.github.com>
2022-02-12 16:07:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "gui tang be cun",
+    introTitle: 'khanh ngan',
+    introDesc: `vk iu oi hom nay la ngay 14/2 tuc la ngay valentine
+    e bic ko`,
+    btnIntro: '^^chut^^',
+    title: 'chi iu e nhiu lam do',
+    desc: 'e ko bic nhiu chung nao dau',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'nhiu luc cung tu hoi tai sao be lai iu chi',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'ma thoi da goi la iu thi het long het da la dc qtam j li do kkk',
+    mess: 'da em iu chi',
+    messDesc: 'nao len hn thi e cho chi di choi nha chi iu',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
